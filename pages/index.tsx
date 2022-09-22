eb1420195e49604f82d04aa6896de390df74a0c6
Muhammad Bin Jamil <binjamil35@gmail.com>
2022-09-23 02:47:39 +0500

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,26 @@ const IndexPage: NextPage = () => {
   return (
     <main className="">
       <Head>
-        <title>Home page</title>
+        <title>Muhammad Bin Jamil</title>
         <meta charSet="UTF-8" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <div className="mx-auto max-w-5xl h-screen flex flex-col">
@@ -23,7 +41,12 @@ const IndexPage: NextPage = () => {
             </h2>
             <p>
               Meanwhile hit me up on{" "}
-              <a className="text-indigo-500" href="mailto:contact@mbinjamil.dev">contact@mbinjamil.dev</a>{" "}
+              <a
+                className="text-indigo-500"
+                href="mailto:contact@mbinjamil.dev"
+              >
+                contact@mbinjamil.dev
+              </a>{" "}
               for any queries!
             </p>
           </div>
