4c539e1acb6c40a988814f5fd2165e8b12703e2c
Muhammad Bin Jamil <binjamil35@gmail.com>
2023-03-29 12:11:19 +0500

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  // markdown: { shikiConfig: { theme: "css-variables" } },
+  site: "https://example.com",
+  integrations: [mdx(), sitemap()],
+});
