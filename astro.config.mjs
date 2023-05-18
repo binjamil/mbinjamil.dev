16a1927af6ccfb27c03ccebbf0eae1036895e0e4
Muhammad Bin Jamil <binjamil35@gmail.com>
2023-05-19 04:41:50 +0500

    Add accessible image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   // markdown: { shikiConfig: { theme: "css-variables" } },
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
-  experimental: {
-    assets: true,
-  },
+  integrations: [mdx(), sitemap(), image()]
 });
