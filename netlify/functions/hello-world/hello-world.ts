66705edc5b355c7d2962ce5e2de34e08893ffb70
Muhammad Bin Jamil <binjamil35@gmail.com>
2023-05-26 09:13:27 +0500

    Add bare planetscale integration

--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,13 +1,18 @@
 import { Handler } from '@netlify/functions'
+import { withPlanetscale } from '@netlify/planetscale';
 
-export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters!
-  console.log({event, context})
+export const handler: Handler = withPlanetscale(async (event, context) => {
+  const {
+    planetscale: { connection },
+  } = context;
+
+  const result = await connection.execute("select * from hello;");
+  const message = result.rows[0]["name"];
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      message
     }),
-  }
-}
+  };
+});
