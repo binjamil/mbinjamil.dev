75ee3d42eea331166c878609a7359df8d40b0ddb
Muhammad Bin Jamil <binjamil35@gmail.com>
2023-05-31 01:18:55 +0500

    Add list of writings/posts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "mbinjamil.dev";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Hey, I'm Muhammad and here I share my software engineering journey!";
