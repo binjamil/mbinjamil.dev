f460c07591d44ffe1d364c7f2ad5a896c298f0ff
Muhammad Bin Jamil <binjamil35@gmail.com>
2023-05-31 01:31:57 +0500

    Fix rss feed generation error

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,13 +4,16 @@ import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
 	const posts = await getCollection('writings');
+	const items = posts
+		.filter(post => post !== undefined)
+		.map((post) => ({
+			...post.data,
+			link: `/writings/${post.slug}/`,
+		}));
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/writings/${post.slug}/`,
-		})),
+		items,
 	});
 }
