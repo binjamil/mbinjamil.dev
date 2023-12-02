7aae5399ea70a882447c203669f762ab90db9117
Muhammad Bin Jamil <binjamil35@gmail.com>
2023-12-02 19:32:51 +0500

    Upgrade to astro v3 with view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
   // markdown: { shikiConfig: { theme: "css-variables" } },
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), image()]
+  integrations: [mdx(), sitemap()]
 });
